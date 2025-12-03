6ff4e638fa447d49376d0f6250d86391b40a5846
Donna May G. Salazar <donnamay.guldo@gmail.com>
2025-12-03 17:43:50 +0800

    Third Activity

---
+++ Operator.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package act.operator;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Operator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+
+        // Compute results
+        int sum = a + b;
+        int difference = a - b; // a minus b
+        int product = a * b;
+
+        // Display results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+
+        scanner.close();
+
+	}
+
+
+}
