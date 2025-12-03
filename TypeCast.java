05a65f18b83cc84398323398aa045559800e0df8
Donna May G. Salazar <donnamay.guldo@gmail.com>
2025-12-03 17:39:28 +0800

    Second Activity

---
+++ TypeCast.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package act.casting;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class TypeCast {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner age = new Scanner(System.in);
+		System.out.println("Enter your age: ");
+		String ageGet = age.nextLine();
+		
+		int intAge = Integer.parseInt(ageGet);
+		double doubleAge = Double.parseDouble(ageGet);
+		
+		System.out.println("Your age in int: " + intAge);
+		System.out.println("Your age in double: " + doubleAge);
+        
+		
+	}
+
+}
