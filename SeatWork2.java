0ebeff497e9c0930dc8e0048ec5f0c99836e6b01
Donna May G. Salazar <donnamay.guldo@gmail.com>
2025-12-04 16:13:29 +0800

    M2 Activity2

---
+++ SeatWork2.java
@@ -0,0 +1,11 @@
+package act.seattwo;
+
+public class SeatWork2 {
+
+	public static void main(String[] args) {
+		WhiteBoard wb = new WhiteBoard();
+		int sum = wb.addNumbers(5, 3);
+		System.out.println("The total is: " + String.valueOf(sum));
+	}
+
+}
