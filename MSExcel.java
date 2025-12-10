56a0aeb08a3bb0b5b87e182d1a340d72b35e156d
Donna May G. Salazar <donnamay.guldo@gmail.com>
2025-12-11 00:54:09 +0800

    M2 Activity5

---
+++ MSExcel.java
@@ -0,0 +1,23 @@
+package act.seatfive;
+
+public class MSExcel extends Program {
+	
+	public MSExcel(String name) {
+		this.setName(name);
+	}
+	
+	@Override
+	public void run() {
+		String message = "Opening MS Excel...";   	
+		System.out.println(message);
+		this.setIsRunning(true);
+		
+	}
+	
+	@Override
+	public void stop() {
+		String message = "Stopping MS Excel...";      		 
+		System.out.println(message);
+		this.setIsRunning(false);
+	}
+}
