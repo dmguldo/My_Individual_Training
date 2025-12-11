b9db3fe243996800a4b675eeb5df33a91e95cfd5
Donna May G. Salazar <donnamay.guldo@gmail.com>
2025-12-11 14:58:28 +0800

    M1 Activity5 upd

--- Loop.java
+++ Loop.java
@@ -1,18 +1,19 @@
 /**
  * 
  */
-package act.loop;
+package act.M1_Activity5;
 
-/**
- * 
- */
+
 public class Loop {
 
-	/**
-	 * @param args
-	 */
+	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	   int sum = 0;
+
+	   for (int i = 1; i <= 50; i++) {
+            sum += i;
+	   }
+	   System.out.println("Sum = " + sum);
 
 	}
 
